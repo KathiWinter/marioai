f5738d1718de320a41e4bc5254ca34f11b9e914e
KathiWinter <kathi.winter@t-online.de>
2022-08-22 11:17:30 +0200

    Changed Keyboard Inputs to control Mario

--- gym-marioai/demo/play.py
+++ gym-marioai/demo/play.py
@@ -9,36 +9,28 @@ from gym_marioai import levels
 all_actions = (0,1,2,3,4,5,6,7,8,9,10,11,12)
 
 env = gym.make('Marioai-v0', render=True,
-               level_path=levels.one_cliff_level,
+               level_path=levels.cliff_level,
                compact_observation=True,
                enabled_actions=all_actions,
                rf_width=20, rf_height=10)
 
 
 def get_action():
-    if keyboard.is_pressed('shift+space+right'):
-        return env.SPEED_JUMP_RIGHT
-    if keyboard.is_pressed('shift+space+left'):
-        return env.SPEED_JUMP_LEFT
-
-    elif keyboard.is_pressed('space+right'):
-        return env.JUMP_RIGHT
-    elif keyboard.is_pressed('space+left'):
-        return env.JUMP_LEFT
-
-    if keyboard.is_pressed('shift+right'):
-        return env.SPEED_RIGHT
-    if keyboard.is_pressed('shift+left'):
-        return env.SPEED_LEFT
-
+    if keyboard.is_pressed('up'):
+        return env.JUMP
     elif keyboard.is_pressed('right'):
-        return env.RIGHT
+        return env.SPEED_RIGHT
     elif keyboard.is_pressed('left'):
-        return env.LEFT
+        return env.SPEED_LEFT
     elif keyboard.is_pressed('down'):
         return env.DOWN
-    elif keyboard.is_pressed('space'):
-        return env.JUMP
+    
+
+    elif keyboard.is_pressed('up+right'):
+        return env.SPEED_JUMP_RIGHT
+    elif keyboard.is_pressed('up+left'):
+        return env.SPEED_JUMP_LEFT
+    
     else:
         return env.NOTHING
 
