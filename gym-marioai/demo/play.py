25f6211f0678b099fb96fbadd517c4979f3f3c29
KathiWinter <kathi.winter@t-online.de>
2022-08-22 16:05:54 +0200

    Changed Keys for Speed_JUMPS

--- gym-marioai/demo/play.py
+++ gym-marioai/demo/play.py
@@ -9,8 +9,8 @@ from gym_marioai import levels
 all_actions = (0,1,2,3,4,5,6,7,8,9,10,11,12)
 
 env = gym.make('Marioai-v0', render=True,
-               level_path=levels.cliff_level,
-               compact_observation=True,
+               level_path=levels.coin_level,
+               compact_observation=False,
                enabled_actions=all_actions,
                rf_width=20, rf_height=10)
 
@@ -26,9 +26,9 @@ def get_action():
         return env.DOWN
     
 
-    elif keyboard.is_pressed('up+right'):
+    elif keyboard.is_pressed('d'):
         return env.SPEED_JUMP_RIGHT
-    elif keyboard.is_pressed('up+left'):
+    elif keyboard.is_pressed('a'):
         return env.SPEED_JUMP_LEFT
     
     else:
@@ -44,8 +44,7 @@ while True:
         a = get_action()
         print('action', a)
         s, r, done, info = env.step(a)
-        # print(len(s), ':', s)
-
+        #print(len(s), ':', s)
         total_reward += r
 
     print(f'finished episode, total_reward: {total_reward}')
