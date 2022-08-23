2977e445511e951eef40755eb9c4d0b47025d923
KathiWinter <kathi.winter@t-online.de>
2022-08-23 21:04:18 +0200

    write dataset for each episode

--- ATAC/play-for-training.py
+++ ATAC/play-for-training.py
@@ -1,8 +1,10 @@
+import os
 import keyboard
 
 import gym
 import gym_marioai
 import numpy as np
+from os.path import exists
 from gym_marioai import levels
 from d3rlpy.dataset import MDPDataset
 
@@ -11,9 +13,12 @@ all_actions = (0,1,2,3,4,5,6,7,8,9,10,11,12)
 
 env = gym.make('Marioai-v0', render=True,
                level_path=levels.coin_level,
-               compact_observation=True,
+               compact_observation=False, #this must stay false for proper saving in dataset
                enabled_actions=all_actions,
                rf_width=20, rf_height=10)
+
+#TODO: Should we save a dataset for each player or for each episode? Or just join all datasets?
+datapath = os.path.join("data", "dataset.h5")
 
 def get_action():
     if keyboard.is_pressed('up'):
@@ -34,18 +39,19 @@ def get_action():
     else:
         return env.NOTHING
 
-#initialize data arrays
-observations = []
-actions = []
-rewards = []
-terminals = []
-
 #play loop: execute actions from keyboard input in the environment
 while True:
+    #new episode
     state = env.reset()
     done = False
     total_reward = 0
     
+    #initialize data arrays with initial states for each episode
+    observations = [state]
+    actions = [12] #nothing
+    rewards = [0]
+    terminals = [done]
+
     while not done:
         action = get_action()
         print('action', action)
@@ -60,13 +66,17 @@ while True:
 
     print(f'finished episode, total_reward: {total_reward}')
     
-    #create Markov-Decision-Process Dataset from collected data
-    dataset = MDPDataset(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))    
-    print(dataset.episodes[0].terminal)
-    #save dataset as file
-    dataset.dump('dataset.h5')
+    #create Markov-Decision-Process Dataset from collected data or append existing MDP Dataset
+    if not exists(datapath):
+        dataset = MDPDataset(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))   
+    else:
+        dataset = MDPDataset.load(datapath)
+        dataset.append(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))
+        
+    #save dataset in file path for episode
+    dataset.dump(datapath)
 
-#we should introduce a proper finish-criterion 
+#TODO: we should introduce a proper finish-criterion 
 print('finished demo')
 
 
