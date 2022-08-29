2af467fbe41120505481681c998e12f30d1cd037
KathiWinter <kathi.winter@t-online.de>
2022-08-29 14:03:09 +0200

    create h5 file for each episode

--- ATAC/play-for-training.py
+++ ATAC/play-for-training.py
@@ -1,5 +1,7 @@
 import os
 import keyboard
+import argparse
+import time
 
 import gym
 import gym_marioai
@@ -8,17 +10,39 @@ from os.path import exists
 from gym_marioai import levels
 from d3rlpy.dataset import MDPDataset
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--level", type=str, default="flatLevel")
+parser.add_argument("-u", "--user", type=int, default=0) #0=testuser
+args = parser.parse_args()
+
+
+def get_level(input=args.level):
+    if input == "easyLevel":
+        return levels.easy_level
+    elif input == "cliffLevel":
+        return levels.cliff_level
+    elif input == "earlyCliffLevel":
+        return levels.early_cliff_level
+    elif input == "coinLevel":
+        return levels.coin_level
+    elif input == "enemyLevel":
+        return levels.enemy_level
+    elif input == "flatLevel":
+        return levels.flat_level
+    elif input == "hardLevel":
+        return levels.hard_level
+    elif input == "oneCLiffLevel":
+        return levels.one_cliff_level
+    else: print("the level you entered does not exist")
+
 
 all_actions = (0,1,2,3,4,5,6,7,8,9,10,11,12)
 
 env = gym.make('Marioai-v0', render=True,
-               level_path=levels.coin_level,
+               level_path=get_level(),
                compact_observation=False, #this must stay false for proper saving in dataset
                enabled_actions=all_actions,
                rf_width=20, rf_height=10)
-
-#TODO: Should we save a dataset for each player or for each episode? Or just join all datasets?
-datapath = os.path.join("data", "dataset.h5")
 
 def get_action():
     if keyboard.is_pressed('up'):
@@ -63,21 +87,18 @@ while True:
         terminals.append(done)
      
         total_reward += reward
-
-    print(f'finished episode, total_reward: {total_reward}')
-    
-    #create Markov-Decision-Process Dataset from collected data or append existing MDP Dataset
-    if not exists(datapath):
-        dataset = MDPDataset(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))   
-    else:
-        dataset = MDPDataset.load(datapath)
-        dataset.append(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))
+        
+    #create Markov-Decision-Process Dataset from collected episode
+    datafile_name = "user" + str(args.user) + "_" + str(args.level) + "_" + "reward" + str(int(total_reward)) + "_" + str(round(time.time())) + ".h5" 
+    datapath = os.path.join("data", datafile_name)
+    dataset = MDPDataset(np.array(observations), np.array(actions), np.array(rewards), np.array(terminals))
         
     #save dataset in file path for episode
     dataset.dump(datapath)
 
-#TODO: we should introduce a proper finish-criterion 
-print('finished demo')
+    print(f'finished episode, total_reward: {total_reward}')
+    
+
 
 
 
