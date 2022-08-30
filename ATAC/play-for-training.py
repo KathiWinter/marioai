bb27ad22c5077580d9f40ec39bf097e66b490571
KathiWinter <kathi.winter@t-online.de>
2022-08-30 12:24:15 +0200

    Added seeds and random seeds - ready to play

--- ATAC/play-for-training.py
+++ ATAC/play-for-training.py
@@ -1,4 +1,5 @@
 import os
+from random import randint
 import keyboard
 import argparse
 import time
@@ -10,8 +11,9 @@ from os.path import exists
 from gym_marioai import levels
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--level", type=str, default="flatLevel")
+parser.add_argument("-l", "--level", type=str)
 parser.add_argument("-u", "--user", type=int, default=0) #0=testuser
+parser.add_argument("-s", "--seed", default=0)
 args = parser.parse_args()
 
 
@@ -32,13 +34,20 @@ def get_level(input=args.level):
         return levels.hard_level
     elif input == "oneCLiffLevel":
         return levels.one_cliff_level
-    else: print("the level you entered does not exist")
+    else:
+        return None
+    
+def get_seed(input=args.seed):
+    if input == "random":
+        seed = randint(0,1000)
+        return seed
+    else:
+        return int(input)
 
 
 all_actions = (0,1,2,3,4,5,6,7,8,9,10,11,12)
 
 env = gym.make('Marioai-v0', render=True,
-               level_path=get_level(),
                compact_observation=False, #this must stay false for proper saving in dataset
                enabled_actions=all_actions,
                rf_width=20, rf_height=10)
@@ -62,10 +71,18 @@ def get_action():
     else:
         return env.NOTHING
 
+level = get_level()
 #play loop: execute actions from keyboard input in the environment
 while True:
     #new episode
-    state = env.reset()
+    if level != None:
+        level_str = str(args.level)
+        state = env.reset(level_path=level)
+    else:
+        seed = get_seed()
+        level_str = str(seed)
+        state = env.reset(seed=get_seed())
+        
     done = False
     total_reward = 0
     
@@ -77,7 +94,6 @@ while True:
 
     while not done:
         action = get_action()
-        print('action', action)
         next_state, reward, done, info = env.step(action)
         
         observations.append(next_state)
@@ -88,7 +104,7 @@ while True:
         total_reward += reward
         
     #create Markov-Decision-Process Dataset from collected episode
-    datafile_name = "user" + str(args.user) + "_" + str(args.level) + "_" + "reward" + str(int(total_reward)) + "_" + str(round(time.time())) 
+    datafile_name = "user" + str(args.user) + "_" + level_str + "_" + "reward" + str(int(total_reward)) + "_" + str(round(time.time())) 
     datapath = os.path.join("data", datafile_name)
     
     data = np.savez(datapath, observations=observations, actions=actions, rewards=rewards, terminals=terminals)
