ec95806e1425378f110bbc054684cf5959f910bd
KathiWinter <kathi.winter@t-online.de>
2022-09-11 22:49:06 +0200

    fixed path error

--- offline_rl/preprocess_data.py
+++ offline_rl/preprocess_data.py
@@ -8,7 +8,7 @@ from constants import DATAPATH
 
 
 
-episode_start = np.load('../data/user0_0_reward162_1662924687.npz')
+episode_start = np.load('data/user0_0_reward162_1662924687.npz')
 
 observation_start = episode_start['observations']
 actions_start = episode_start['actions']
@@ -24,7 +24,7 @@ dataset.dump(DATAPATH)
 
 counter = 0
 #create Markov-Decision-Process Dataset from collected data or append existing MDP Dataset
-for file in glob.glob(os.path.join('../data/', '*.npz')):
+for file in glob.glob(os.path.join('data/', '*.npz')):
     episode_data = np.load(file)
         
     observations = episode_data["observations"]
